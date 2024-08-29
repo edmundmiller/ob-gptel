1bd490165cad9347b4b65c8d11a753607961db94
Edmund Miller <git@edmundmiller.dev>
2024-08-29 17:40:42 -0500

    Regen

--- ob-gptel.el
+++ ob-gptel.el
@@ -68,99 +68,37 @@
 (require 'gptel)
 
 ;; optionally declare default header arguments for this language
-(defvar org-babel-default-header-args:gptel '())
+(defvar org-babel-default-header-args:gptel
+  '((:model . "gpt-3.5-turbo")
+    (:system-message . "You are a helpful assistant."))
+  "Default arguments for evaluating a gptel source block.")
 
-;; This function expands the body of a source code block by doing things like
-;; prepending argument definitions to the body, it should be called by the
-;; `org-babel-execute:gptel' function below. Variables get concatenated in
-;; the `mapconcat' form, therefore to change the formatting you can edit the
-;; `format' form.
-(defun org-babel-expand-body:gptel (body params &optional processed-params)
-  "Expand BODY according to PARAMS, return the expanded body."
-  (require 'inf-gptel nil t)
-  (let ((vars (org-babel--get-vars (or processed-params (org-babel-process-params params)))))
-    (concat
-     (mapconcat ;; define any variables
-      (lambda (pair)
-        (format "%s=%S"
-                (car pair) (org-babel-gptel-var-to-gptel (cdr pair))))
-      vars "\n")
-     "\n" body "\n")))
+(defcustom ob-gptel-default-model "gpt-3.5-turbo"
+  "Default model to use when executing a gptel source block."
+  :group 'org-babel
+  :type 'string)
 
-;; This is the main function which is called to evaluate a code
-;; block.
-;;
-;; This function will evaluate the body of the source code and
-;; return the results as emacs-lisp depending on the value of the
-;; :results header argument
-;; - output means that the output to STDOUT will be captured and
-;;   returned
-;; - value means that the value of the last statement in the
-;;   source code block will be returned
-;;
-;; The most common first step in this function is the expansion of the
-;; PARAMS argument using `org-babel-process-params'.
-;;
-;; Please feel free to not implement options which aren't appropriate
-;; for your language (e.g. not all languages support interactive
-;; "session" evaluation).  Also you are free to define any new header
-;; arguments which you feel may be useful -- all header arguments
-;; specified by the user will be available in the PARAMS variable.
+(defcustom ob-gptel-default-system-message "You are a helpful assistant."
+  "Default system message to use when executing a gptel source block."
+  :group 'org-babel
+  :type 'string)
+
 (defun org-babel-execute:gptel (body params)
-  "Execute a block of gptel code with org-babel.
-This function is called by `org-babel-execute-src-block'"
-  (message "executing gptel source code block")
+  "Execute a block of GPTel code with org-babel."
   (let* ((processed-params (org-babel-process-params params))
-         ;; set the session if the value of the session keyword is not the
-         ;; string `none'
-         (session (unless (string= value "none")
-                    (org-babel-gptel-initiate-session
-                     (cdr (assq :session processed-params)))))
-         ;; variables assigned for use in the block
-         (vars (org-babel--get-vars processed-params))
-         (result-params (assq :result-params processed-params))
-         ;; either OUTPUT or VALUE which should behave as described above
-         (result-type (assq :result-type processed-params))
-         ;; expand the body with `org-babel-expand-body:gptel'
-         (full-body (org-babel-expand-body:gptel
-                     body params processed-params)))
-    ;; actually execute the source-code block either in a session or
-    ;; possibly by dropping it to a temporary file and evaluating the
-    ;; file.
-    ;;
-    ;; for session based evaluation the functions defined in
-    ;; `org-babel-comint' will probably be helpful.
-    ;;
-    ;; for external evaluation the functions defined in
-    ;; `org-babel-eval' will probably be helpful.
-    ;;
-    ;; when forming a shell command, or a fragment of code in some
-    ;; other language, please preprocess any file names involved with
-    ;; the function `org-babel-process-file-name'. (See the way that
-    ;; function is used in the language files)
-    ))
-
-;; This function should be used to assign any variables in params in
-;; the context of the session environment.
-(defun org-babel-prep-session:gptel (session params)
-  "Prepare SESSION according to the header arguments specified in PARAMS."
-  )
-
-(defun org-babel-gptel-var-to-gptel (var)
-  "Convert an elisp var into a string of gptel source code
-specifying a var of the same value."
-  (format "%S" var))
-
-(defun org-babel-gptel-table-or-string (results)
-  "If the results look like a table, then convert them into an
-Emacs-lisp table, otherwise return the results as a string."
-  )
-
-(defun org-babel-gptel-initiate-session (&optional session)
-  "If there is not a current inferior-process-buffer in SESSION then create.
-Return the initialized session."
-  (unless (string= session "none")
-    ))
+         (model (or (cdr (assq :model processed-params))
+                    ob-gptel-default-model))
+         (system-message (or (cdr (assq :system-message processed-params))
+                             ob-gptel-default-system-message)))
+    (condition-case err
+        (gptel-request
+            body
+          :system system-message
+          :model model
+          :callback (lambda (response _)
+                      response))
+      (error
+       (format "GPTel request failed: %s" (error-message-string err))))))
 
 (provide 'ob-gptel)
 ;;; ob-gptel.el ends here
